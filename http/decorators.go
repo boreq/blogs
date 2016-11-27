6a9474a5c846dd06185102a5108e0f84c623136b
boreq <boreq@0x46.net>
2016-11-27 23:59:16 +0100

    http: moved from utils/http

--- http/decorators.go
+++ http/decorators.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"github.com/boreq/blogs/http/context"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
@@ -15,5 +16,17 @@ func WithErrorHandling(eh ErrorHandler) httprouter.Handle {
 		if err := eh(w, r, p); err != nil {
 			http.Error(w, err.Error(), 500)
 		}
+	}
+}
+
+// NotAuthenticatedOnly redirects authenticated users to the home page.
+func NotAuthenticatedOnly(h httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		ctx := context.Get(r)
+		if ctx.User.IsAuthenticated() {
+			http.Redirect(w, r, "/", 301)
+			return
+		}
+		h(w, r, p)
 	}
 }
