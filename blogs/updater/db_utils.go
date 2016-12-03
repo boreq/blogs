6471b9d3b149c2045e901c6c39984c226d595d75
boreq <boreq@0x46.net>
2016-12-04 00:58:50 +0100

    blogs/updater: formatting

--- blogs/updater/db_utils.go
+++ blogs/updater/db_utils.go
@@ -39,7 +39,8 @@ func getCategory(tx *sqlx.Tx, blog database.Blog, name string) (database.Categor
 
 func getPost(tx *sqlx.Tx, category database.Category, internalID string) (database.Post, error) {
 	_, err := tx.Exec(
-		"INSERT INTO post (category_id, internal_id, title, summary, date) VALUES ($1, $2, $3, $4, $5)",
+		`INSERT INTO post (category_id, internal_id, title, summary, date)
+		VALUES ($1, $2, $3, $4, $5)`,
 		category.ID, internalID, "", "", time.Time{})
 	if err != nil && !isUniqueConstraintError(err) {
 		log.Printf("getPost, error: %s", err.Error())
@@ -53,7 +54,12 @@ func getPost(tx *sqlx.Tx, category database.Category, internalID string) (databa
 
 func getTags(post database.Post) ([]database.Tag, error) {
 	var tags []database.Tag
-	err := database.DB.Select(&tags, "SELECT tag.* FROM tag JOIN post_to_tag ON post_to_tag.tag_id = tag.id JOIN post ON post.id = post_to_tag.post_id WHERE post.id=$1", post.ID)
+	err := database.DB.Select(&tags,
+		`SELECT tag.*
+		FROM tag
+		JOIN post_to_tag ON post_to_tag.tag_id=tag.id
+		JOIN post ON post.id=post_to_tag.post_id
+		WHERE post.id=$1`, post.ID)
 	return tags, err
 }
 
@@ -74,12 +80,12 @@ type postWithCategory struct {
 
 func getPostsForDeleteOldPosts(blog database.Blog) ([]postWithCategory, error) {
 	var posts []postWithCategory
-	err := database.DB.Select(&posts, `
-	SELECT post.*, category.*
-	FROM post
-	JOIN category ON post.category_id=category.id
-	JOIN blog ON category.blog_id=blog.id
-	WHERE blog.id=$1`, blog.ID)
+	err := database.DB.Select(&posts,
+		`SELECT post.*, category.*
+		FROM post
+		JOIN category ON post.category_id=category.id
+		JOIN blog ON category.blog_id=blog.id
+		WHERE blog.id=$1`, blog.ID)
 	return posts, err
 }
 
