3de4a472d571957db2d91427299f800d414f77d1
boreq <boreq@0x46.net>
2016-12-03 01:07:01 +0100

    http: removed WithErrorHandling

--- http/decorators.go
+++ http/decorators.go
@@ -6,19 +6,6 @@ import (
 	"net/http"
 )
 
-type ErrorHandler func(http.ResponseWriter, *http.Request, httprouter.Params) error
-
-// WithErrorHandling converts an ErrorHandler into a normal httprouter handle
-// adding error handling to it. If the wrapped function returns an error or
-// panics an error page is displayed.
-func WithErrorHandling(eh ErrorHandler) httprouter.Handle {
-	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		if err := eh(w, r, p); err != nil {
-			http.Error(w, err.Error(), 500)
-		}
-	}
-}
-
 // NotAuthenticatedOnly redirects authenticated users to the home page.
 func NotAuthenticatedOnly(h httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
