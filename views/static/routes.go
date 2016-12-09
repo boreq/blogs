c3bcaee091c7d3376ab695f73b1245041bdfc726
boreq <boreq@0x46.net>
2016-12-10 00:50:28 +0100

    views/static: replaced a hardcoded value with a config parameter

--- views/static/routes.go
+++ views/static/routes.go
@@ -1,10 +1,11 @@
 package static
 
 import (
+	"github.com/boreq/blogs/config"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
 func Register(router *httprouter.Router) {
-	router.ServeFiles("/static/*filepath", http.Dir("_static/"))
+	router.ServeFiles("/static/*filepath", http.Dir(config.Config.StaticDirectory))
 }
