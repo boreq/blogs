608685152925fbff2aa7fe64c0dbf1529df1bcc5
boreq <boreq@0x46.net>
2016-12-12 23:04:47 +0100

    blogs/common: DownloadAndParse sets a User-Agent to confuse naughty websites that don't want to be parsed

--- blogs/common/common.go
+++ blogs/common/common.go
@@ -5,14 +5,29 @@ import (
 	"fmt"
 	htmlutils "github.com/boreq/blogs/utils/html"
 	"golang.org/x/net/html"
+	"math/rand"
 	"net/http"
 )
+
+// IoS
+var userAgents = []string{
+	"Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0",
+	"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36",
+	"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.100 Safari/537.36",
+}
 
 // DownloadAndParse downloads a web page using a GET request and parses the
 // downloaded HTML.
 func DownloadAndParse(url string) (*html.Node, error) {
+	// Create the request (set a random user agent)
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Set("User-Agent", userAgents[rand.Intn(len(userAgents))])
+
 	// Download the page
-	resp, err := http.Get(url)
+	resp, err := http.DefaultClient.Do(request)
 	if err != nil {
 		return nil, err
 	}
