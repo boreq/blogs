159a547eff29bd4196fbdda46289ad6a819d6444
boreq <boreq@0x46.net>
2016-12-12 21:41:48 +0100

    http: added AuthenticatedOnly

--- http/decorators.go
+++ http/decorators.go
@@ -17,3 +17,15 @@ func NotAuthenticatedOnly(h httprouter.Handle) httprouter.Handle {
 		h(w, r, p)
 	}
 }
+
+// AuthenticatedOnly redirects not authenticated users to the home page.
+func AuthenticatedOnly(h httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		ctx := context.Get(r)
+		if !ctx.User.IsAuthenticated() {
+			http.Redirect(w, r, "/", 301)
+			return
+		}
+		h(w, r, p)
+	}
+}
