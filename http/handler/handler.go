a416db96f12be7f4bbd2d1d8d7ec0d5b986c70c1
boreq <boreq@0x46.net>
2016-12-14 23:06:52 +0100

    http/handler: wrapped with RecoverHandler

--- http/handler/handler.go
+++ http/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	bhttp "github.com/boreq/blogs/http"
 	"github.com/boreq/blogs/http/context"
 	"github.com/boreq/blogs/views"
 	"github.com/boreq/blogs/views/errors"
@@ -8,11 +9,12 @@ import (
 	"net/http"
 )
 
+// Get returns the http handler used by the blogs server.
 func Get() http.Handler {
 	router := httprouter.New()
 	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		errors.NotFound(w, r)
 	})
 	views.Register(router)
-	return context.ClearHandler(router)
+	return bhttp.RecoverHandler(context.ClearHandler(router))
 }
